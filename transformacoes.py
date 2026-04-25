4ca095c2f2dd991f7344765e972315ea0ecd35df
Gabriel Stamford <gabrielstamford21@hotmail.com>
2026-04-24 23:42:05 -0300

    add transformacoes em vetores

--- transformacoes.py
+++ transformacoes.py
@@ -48,8 +48,25 @@ def matriz_rotacao_z(angulo_graus: float) -> np.ndarray:
         [0.0, 0.0,  0.0, 1.0]
     ], dtype=np.float64)
 
+
 def aplicar_matriz_ponto(matriz: np.ndarray, ponto: Ponto) -> Ponto:
     """Aplica uma matriz 4x4 a um Ponto (x, y, z, 1)."""
     p_homogeneo = np.array([ponto.x, ponto.y, ponto.z, 1.0], dtype=np.float64)
     p_transformado = matriz @ p_homogeneo
     return Ponto(p_transformado[0], p_transformado[1], p_transformado[2])
+
+
+def aplicar_matriz_vetor(matriz: np.ndarray, vetor: Vetor) -> Vetor:
+    """
+    Aplica uma matriz 4x4 a um Vetor (x, y, z, 0).
+    Como a 4ª componente é 0, a translação é automaticamente ignorada,
+    mas a rotação e a escala são aplicadas.
+    """
+    # Note o 0.0 no final, que caracteriza matematicamente um vetor direcional
+    v_homogeneo = np.array([vetor.x, vetor.y, vetor.z, 0.0], dtype=np.float64)
+    
+    # Multiplicação da matriz pelo vetor
+    v_transformado = matriz @ v_homogeneo
+    
+    # Retorna como nossa classe Vetor
+    return Vetor(v_transformado[0], v_transformado[1], v_transformado[2])
